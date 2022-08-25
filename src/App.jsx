cb113224d5520224c24fc5a459958472b07ff155
ayogsynergy <sanchezg.eng@gmail.com>
2022-08-24 23:00:03 -0400

    Firing the granularity change. Make note that StrictMode renders components twice to help find errors. It leads to double subscriptions, however.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { from, map } from "rxjs";
 import './App.css'
+import {piGranularity$} from "./piGranularity.js";
+
+let piGranularityObserver = piGranularity$.pipe(
+    map((val) => val)
+)
 
 function App() {
   const [piProjections, setPiProjections] = useState([]);
+
+  const onPiGranularityChange = (granularity) => {
+    console.log(granularity);
+  };
+    useEffect(() => {
+        // To consume an observable, subscribe.
+        let subscription = piGranularityObserver.subscribe((result) => {
+            onPiGranularityChange(result)
+        });
+        return () => subscription.unsubscribe();
+    }, []);
 
   return (
     <div className="App">
---
+++ src/piGranularity.js
@@ -0,0 +1,3 @@
+import { from } from "rxjs";
+
+export const piGranularity$ = from([0,1,2,3,4]);
